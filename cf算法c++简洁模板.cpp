904ebaacd96ff531fbf67805267d9ac68be8233c
ddbjiang <167593237+ddbjiang@users.noreply.github.com>
2025-08-25 19:01:12 +0800

    Add files via upload

---
+++ cf算法c++简洁模板.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define mb emplace_back
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+const ll mod = 1000000007;
+//const ll mod = 998244353;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vd = vector<double>;
+using vvi = vector<vector<int> >;
+using vvl = vector<vector<ll> >;
+using vvvi = vector<vector<vector<int>>>;
+using vvvl = vector<vector<vector<ll>>>;
+using vpii = vector<pair<int, int> >;
+using vpll = vector<pair<ll, ll> >;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define go(x) int t; cin >> t; while(t--) x();
+template<typename T>
+void db(const T& x) { cout << "debug: " << x << endl; }
+template<typename T>
+void db(const vector<T>& v) { cout << "debug(1): "; for (const auto& i : v)cout << i << " "; cout << endl; }
+template <typename T>
+void db(const vector<vector<T>>& v) { cout << "debug(2): " << endl; for (const auto& i : v) { db(i); } }
+void solve() {
+    //db("-----------");
+    
+
+    
+}
+
+
+/*
+
+
+
+*/
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);cout.tie(nullptr);
+    go(solve);
+    //solve();
+    return 0;
+}
