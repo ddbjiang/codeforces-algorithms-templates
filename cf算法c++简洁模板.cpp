9e31ca17074924f2c2ec7534a0bf5edf70802e5f
ddbjiang <167593237+ddbjiang@users.noreply.github.com>
2025-09-17 20:44:15 +0800

    Update cf算法c++简洁模板.cpp

--- cf算法c++简洁模板.cpp
+++ cf算法c++简洁模板.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #define endl "\n"
 #define all(a) a.begin(), a.end()
 #define rall(a) a.rbegin(), a.rend()
+#define int long long
 #define ll long long
 #define ull unsigned long long
 #define pb push_back
@@ -12,25 +13,20 @@ using namespace std;
 const ll mod = 1000000007;
 //const ll mod = 998244353;
 using vi = vector<int>;
-using vl = vector<ll>;
 using vd = vector<double>;
 using vvi = vector<vector<int>>;
-using vvl = vector<vector<ll>>;
 using vvvi = vector<vector<vector<int>>>;
-using vvvl = vector<vector<vector<ll>>>;
 using vpii = vector<pair<int, int>>;
-using vpll = vector<pair<ll, ll>>;
 using pii = pair<int, int>;
-using pll = pair<ll, ll>;
 #define go(x) int t; cin >> t; while(t--) x();
 template<typename T>
-void db(T& x) {cout << x;}
+void db(T& x) { cout << x; }
 template<typename T>
-void db(vector<T>& v){cout << "[";for (auto &x : v) cout << (&x==&v[0]?"":",") << x;cout << "]";}
+void db(vector<T>& v) { cout << "[";for (auto& x : v) cout << (&x == &v[0] ? "" : ",") << x;cout << "]"; }
 template<typename T>
-void db(vector<vector<T>>& v) {cout << endl;for (auto& i : v) {db(i);cout << endl;}}
+void db(vector<vector<T>>& v) { cout << endl;for (auto& i : v) { db(i);cout << endl; } }
 template<typename T>
-void debug(char* name, T& x) {cout << "debug: " << name << " = ";db(x);cout << endl;}
+void debug(char* name, T& x) { cout << "debug: " << name << " = ";db(x);cout << endl; }
 #define db(x) debug(#x, x)
 
 void solve() {
@@ -46,7 +42,7 @@ void solve() {
 */
 
 
-int main() {
+signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);cout.tie(nullptr);
     go(solve);
