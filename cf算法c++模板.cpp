88ba909c66d8b87fb070b80dbb84feb79bf89a81
ddbjiang <167593237+ddbjiang@users.noreply.github.com>
2025-09-19 16:48:36 +0800

    Update cf算法c++模板.cpp

--- cf算法c++模板.cpp
+++ cf算法c++模板.cpp
@@ -18,7 +18,6 @@ using vvi = vector<vector<int>>;
 using vvvi = vector<vector<vector<int>>>;
 using vpii = vector<pair<int, int>>;
 using pii = pair<int, int>;
-#define go(x) int t; cin >> t; while(t--) x();
 template<typename T>
 void db(T& x) { cout << x; }
 template<typename T>
@@ -247,7 +246,11 @@ signed main() {
     //initny(MAXN);
     //initjc(MAXN);
     //initp(MAXN);
-    go(solve);
-    //solve();
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; i++) {
+        //cout << "----Test " << i << "----" << endl;
+        solve();
+    }
     return 0;
 }
