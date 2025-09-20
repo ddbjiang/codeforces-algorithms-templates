406fcf24d497d4e86060e59b0820bce1b95bc362
ddbjiang <167593237+ddbjiang@users.noreply.github.com>
2025-09-20 11:29:00 +0800

    Add files via upload

---
+++ duipai.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    system("g++ -std=c++11 rand.cpp -o rand.exe");
+    system("g++ -std=c++11 t1.cpp -o t1.exe");
+    system("g++ -std=c++11 t2.cpp -o t2.exe");
+    int t=1;
+    while (true) {
+    	cout<<"test "<<t++<<":";
+        system("rand.exe > data.txt");
+        system("t1.exe < data.txt > t1.txt");
+        system("t2.exe < data.txt > t2.txt");
+        if (system("fc t1.txt t2.txt")) {
+            cout << "wa" << endl;
+            cout<<"data:"<<endl;
+            system("type data.txt");
+            break;
+        }
+        else {
+            cout << "ac" << endl;
+        }
+    }
+    return 0;
+}
