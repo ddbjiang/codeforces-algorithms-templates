61584b4277411ca341ae4f671ca27a042269c49d
ddbjiang <167593237+ddbjiang@users.noreply.github.com>
2026-04-08 20:47:00 +0800

    Update cf算法c++简洁模板.cpp

--- cf算法c++简洁模板.cpp
+++ cf算法c++简洁模板.cpp
@@ -5,6 +5,10 @@ using namespace std;
 #define rall(a) a.rbegin(), a.rend()
 #define int long long
 #define pb push_back
+#define eb emplace_back
+#define pf push_front
+#define fi first
+#define se second
 #define sz(a) (int)(a).size()
 #define yes cout<<"Yes"<<endl
 #define no cout<<"No"<<endl
@@ -13,11 +17,9 @@ using namespace std;
 #define rso(a) sort(rall(a))
 using ll = long long;using ull = unsigned long long;using ld = long double;using i128 = __int128;using u128 = unsigned __int128;
 using vi = vector<int>;using vl = vector<ll>;using vvi = vector<vi>;using vvl = vector<vl>;using vvvi = vector<vvi>;using vvvl = vector<vvl>;
-using vd = vector<double>;using vs = vector<string>;using pii = pair<int, int>;using vpii = vector<pii>;
 using si = set<int>;using msi = multiset<int>;using mii = map<int, int>;
-using ar3 = array<int, 3>;
-const ll mod = 1000000007;//const ll mod = 998244353;
-const double pai = acos(-1.0), eps = 1e-9;
+using vd = vector<double>;using vs = vector<string>;using pii = pair<int, int>;using vpii = vector<pii>;using vvpii = vector<vpii>;
+using ar3 = array<int, 3>;using tiii = tuple<int, int, int>;//get<0>(t);
 template<class T> void debug(const T& x) { cout << x; }
 template<class T> void debug(const vector<T>& v) { cout << "[";for (int i = 0, n = v.size();i < n;i++)cout << v[i] << ",]"[i + 1 == n]; }
 template<class T> void debug(const vector<vector<T>>& v) { cout << endl;for (const auto& i : v) { debug(i);cout << endl; } }
@@ -47,8 +49,14 @@ struct node {
         if (x == o.x) return y < o.y;return x < o.x;
     }
 };
+const ll mod = 1000000007;//const ll mod = 998244353;
+const ll base1 = 131, base2 = 13331, smod1 = 1e9 + 7, smod2 = 1e9 + 9;
+const double pai = acos(-1.0), eps = 1e-9;
+const int N = 2e5 + 10;
+
 
 void solve() {
+
 
 
 }
